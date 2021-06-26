c65cf7deaafe552aa8914f7d5cfd2ee9d5aedc47
Prateek Bahukhandi <bahukhandiprateek@gmail.com>
2021-06-27 03:54:23 +0530

    Added Merge Two Binary tree code

---
+++ Leetcode/MergeTwoBinaryTree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+
+        if (root1 == NULL && root2 == NULL)
+            return NULL;
+        else if (root1 != NULL && root2 == NULL)
+            return root1;
+        else if (root2 != NULL && root1 == NULL)
+            return root2;
+        else
+            root1->val += root2->val;
+
+
+
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right = mergeTrees(root1->right, root2->right);
+
+        return root1;
+
+    }
+
+
+
+};
