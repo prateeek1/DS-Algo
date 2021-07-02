34efa6d07a7bde9198b3c1f4bce6a1c992dd5c5d
Prateek Bahukhandi <bahukhandiprateek@gmail.com>
2021-07-02 21:22:20 +0530

    Made changes in the code for LCA of node

--- BinaryTree/LCAofNode.cpp
+++ BinaryTree/LCAofNode.cpp
@@ -1,96 +1,21 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-class node
+Node* lca(Node* root , int n1 , int n2 )
 {
-public:
-	int data;
-	node* left;
-	node* right;
+   if (root == NULL)
+      return NULL;
 
-	node(int d)
-	{
-		data = d;
-		left == NULL;
-		right == NULL;
-	}
-};
-
-node* build()
-{
-	int d;
-	cin >> d;
-	if (d == -1)
-	{
-		return NULL;
-	}
-	node* root = new node(d);
-	root->left = build();
-	root->right = build();
-
-	return root;
-}
+   if (root->data == n1 || root->data == n2) // If we find either n1 or n2 in node then this node id the LCA
+      return root;
 
 
-node* lca(node*root, int a, int b)
-{
-	if (root == NULL)
-		return NULL;
+   Node* left = lca(root->left, n1, n2); // Traverse left and right child
+   Node* right = lca(root->right, n1, n2);
 
-	if (root->data == a or root->data == b)
-		return root;
+   if (left != NULL && right != NULL) // If right and left both are not NULL then Lca is this node as left and right contains n1 and n2
+      return root;
 
-	node* leftans = lca(root->left, a, b);
-	node* rightans = lca(root->right, a, b);
+   if (left != NULL) // If n2 is not found left is Lca and vice versa
+      return left;
+   else
+      return right;
 
-	if (leftans != NULL && rightans != NULL)
-		return root;
-
-	if (leftans != NULL)
-		return leftans;
-	else
-		return rightans;
-}
-
-
-// LEVEL OF THE GIVEN NODE
-int search(node*root, int key, int level)
-{
-	if (root == NULL)
-		return -1;
-
-	if (root->data == key)
-		return level;
-
-	int left = search(root->left, key, level + 1);
-	if (left != -1)
-		return level;
-	return search(root->right, key, level + 1);
-
-}
-
-//Function to find distance between two nodes
-int findDistance(node*root, int a, int b)
-{
-	node* lca_node = lca(root, a, b);
-	int l1 = search(lca_node, a, 0);
-	int l2 = search(lca_node, b, 0);
-	return l1 + l2;
-
-}
-
-void print(node*root)
-{
-	if (root == NULL)
-		return;
-
-	cout << root->data << " ";
-	print(root->left);
-	print(root->right);
-}
-
-int main()
-{
-	node*root = build();
-	cout << findDistance(root, 10, 7);
 }
