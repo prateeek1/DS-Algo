e4d85c21eba795fadb9ff0e9b0a10a13dc02af31
Prateek Bahukhandi <bahukhandiprateek@gmail.com>
2021-07-25 15:48:31 +0530

    Added code for Leetcode problem

---
+++ Leetcode/LinkedList/AddTwoNumbers.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+    int carry = 0;
+    ListNode* l3 = new ListNode(); // Make a New linked list
+    ListNode*temp = l3; // Temp to iterate the list
+
+    while (l1 != NULL || l2 != NULL || carry)
+    {
+      int sum = 0;
+      if (l1 != NULL)
+      {
+        sum += l1->val;
+        l1 = l1->next;
+      }
+
+      if (l2 != NULL)
+      {
+        sum += l2->val;
+        l2 = l2->next;
+      }
+
+      sum += carry;
+      carry = sum / 10;
+      ListNode* node = new ListNode(sum % 10);
+      temp->next = node;
+      temp = temp->next;
+    }
+
+    return l3->next;
+
+  }
+};
